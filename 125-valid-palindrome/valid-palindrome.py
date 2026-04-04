c649d3fbe3a93a2b41d6bd92f02e5f713251482e
andsuperme <andsuperme@gmail.com>
2026-04-04 16:03:13 -0700

    Time: 15 ms (10.42%) | Memory: 24.1 MB (5.19%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,9 +1,9 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        alpha = "abcdefghijklmnopqrstuvwxyz0123456789"
         res = []
 
-        for char in s:
-            if char.isalnum():
-                res.append(char.lower())
-
+        for i in range(len(s)):
+            if s[i].lower() in alpha:
+                res.append(s[i].lower())
         return res == res[::-1]
