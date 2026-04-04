1e1943290108273a54b19bce47fe7af16b19ae10
andsuperme <andsuperme@gmail.com>
2026-04-03 22:27:35 -0700

    Time: 0 ms (100.00%) | Memory: 20.3 MB (63.13%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        hashmap = {}
-        
-        for i, num in enumerate(nums): # assigns each value an index
-            if target - num in hashmap: # checks for each num the remainder is already in dictionary
-                return [i, hashmap[target - num]]
-            hashmap[num] = i # adds current number to hashmap
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hm = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] in hm:
+                return [i, hm[target-nums[i]]]
+            hm[nums[i]] = i  
